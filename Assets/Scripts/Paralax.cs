37b37b8a277f555e134f44a29bc4d80b0633f317
Adrian <a25adrianrs@iessanclemente.net>
2025-12-22 17:50:41 +0100

    Parallax Effect

--- Assets/Scripts/Paralax.cs
+++ Assets/Scripts/Paralax.cs
@@ -4,17 +4,26 @@ using UnityEngine.Timeline;
 public class Paralax : MonoBehaviour
 {
     // Campo serializado donde definiremos el efecto paralax, velocidad de scroll de la superficie concreta
-    [SerializeField] float paralax;
+    [SerializeField] float parallax;
     Material mat;
+
+    // Componente transform de la camara
+    Transform cam;
+    Vector3 initialPos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         mat = GetComponent<SpriteRenderer>().material;
+        cam = Camera.main.transform;
+        initialPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        mat.mainTextureOffset += new Vector2(paralax * Time.deltaTime, 0);
+        // Sera la posición x e y de la imagen en cuestión
+        transform.position = new Vector3(cam.position.x, initialPos.y, initialPos.x);
+
+        mat.mainTextureOffset = new Vector2(cam.position.x * parallax, 0);
     }
 }
