31e7545a3fe56e49646e54bb945e5c39a6bc1b21
Adrian <a25adrianrs@iessanclemente.net>
2025-12-29 13:40:09 +0100

    jump mechanic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,20 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed;
-
+    [SerializeField] float jumpSpeed;
     Rigidbody2D rb;
+    // Para implementar el salto y detectar si el personaje esta tocando una de las capas (IsTouchingLayers),
+    // necesitamos obtener una referencia al collider
+    Collider2D col;
     float moveX;
-
+    bool jump;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
     }
 
     void FixedUpdate()
@@ -18,6 +23,8 @@ public class PlayerController : MonoBehaviour
         // usar este metodo que se actualiza con una velocidad de refreso constante
 
         Run();
+        Flip();
+        Jump();
     }
 
 
@@ -30,6 +37,11 @@ public class PlayerController : MonoBehaviour
 
         // (1) = tecla derecha, (-1) = tecla izquierda, (0)= ninguna tecla
         moveX = Input.GetAxisRaw("Horizontal");
+        // Accedemos a uno de los ejes y accedemos a la tecla que Unity ya tiene asignada para tal acción
+        // si la variable booleana para controlar el salto es "false" y estamos pulsando la tecla
+        // asignada a la acción de salto entonces cambiamos el valor del booleano a "true"
+        if (!jump && Input.GetButtonDown("Jump"))
+            jump = true;
 
     }
 
@@ -37,5 +49,21 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 vel1 = new Vector2(moveX * speed * Time.deltaTime, rb.linearVelocity.y);
         rb.linearVelocity = vel1;
+    }
+
+    void Flip()
+    {
+        float speed_x = rb.linearVelocity.x;
+        if (Math.Abs(speed_x) > Mathf.Epsilon)
+            transform.localScale = new Vector2(Mathf.Sign(speed_x), 1);
+    }
+
+    void Jump()
+    {
+        if (jump)
+        {
+            jump = false;
+            rb.linearVelocity += new Vector2(0, jumpSpeed);
+        }
     }
 }
